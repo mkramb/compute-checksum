c4e1f3a89d17e4620d768ee9bcc30119af7096bd
Mitja Kramberger <mitja.kramberger@gmail.com>
2024-10-01 07:23:14 +0100

    Better file collecting

--- src/files.rs
+++ src/files.rs
@@ -1,11 +1,13 @@
 use walkdir::WalkDir;
+use std::ops::Not;
 
 pub fn collect_files(dir: &str) -> Vec<String> {
     let entries = WalkDir::new(&dir)
-        .follow_links(true)
+        .follow_links(false)
         .into_iter()
         .filter_map(|e| e.ok())
         .map(|e| e.path().to_str().unwrap_or("").to_string())
+        .filter(|e| e.is_empty().not())
         .collect::<Vec<String>>();
 
     entries
